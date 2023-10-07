87a8f878f33ff08ceac289577bce79e6edce19ad
Deepak <deepak2603dk@gmail.com>
2023-10-07 15:23:16 +0530

    fourth commit

--- props-day2/src/App.jsx
+++ props-day2/src/App.jsx
@@ -8,14 +8,14 @@ function App() {
           <h1 className='bg-slate-700 py-3 text-center text-lime-50  text-4xl font-extrabold'>Top Movies</h1>
 
     <div className=" min-h-full h-[auto]  flex 	justify-evenly  flex-wrap p-5  bg-slate-500">
-    <Card title={movies[0].Title} Plot= {movies[0].Plot} img = {movies[0].Images[1]}/>
-    <Card title={movies[1].Title} Plot= {movies[1].Plot} img = {movies[1].Images[1]}/>
-    <Card title={movies[2].Title} Plot= {movies[2].Plot} img = {movies[2].Images[1]}/>
-    <Card title={movies[3].Title} Plot= {movies[3].Plot} img = {movies[3].Images[1]}/>
-    <Card title={movies[4].Title} Plot= {movies[4].Plot} img = {movies[4].Images[1]}/>
-    <Card title={movies[5].Title} Plot= {movies[5].Plot} img = {movies[5].Images[1]}/>
-    <Card title={movies[6].Title} Plot= {movies[6].Plot} img = {movies[6].Images[1]}/>
-    <Card title={movies[7].Title} Plot= {movies[7].Plot} img = {movies[7].Images[1]}/>
+    <Card title={movies[0].Title} Plot= {movies[0].Plot} img = {movies[0].Poster}/>
+    <Card title={movies[1].Title} Plot= {movies[1].Plot} img = {movies[1].Poster}/>
+    <Card title={movies[2].Title} Plot= {movies[2].Plot} img = {movies[2].Poster}/>
+    <Card title={movies[3].Title} Plot= {movies[3].Plot} img = {movies[3].Poster}/>
+    <Card title={movies[4].Title} Plot= {movies[4].Plot} img = {movies[4].Poster}/>
+    <Card title={movies[5].Title} Plot= {movies[5].Plot} img = {movies[5].Poster}/>
+    <Card title={movies[6].Title} Plot= {movies[6].Plot} img = {movies[6].Poster}/>
+    <Card title={movies[7].Title} Plot= {movies[7].Plot} img = {movies[7].Poster}/>
     
     </div>
   
--- props-day2/src/Components/Card.jsx
+++ props-day2/src/Components/Card.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 function card(props) {
   return (
     <>
-   <div class="relative h-[400px] w-[300px] mx-2  my-2 rounded-md  ">
+   <div className="relative h-[400px] w-[300px] mx-2  my-2 rounded-md  ">
   <img
     src={props.img}
     alt="poster"
-    class="z-0 h-full w-full rounded-md object-cover"
+    className="z-0 h-full w-full rounded-md object-cover"
   />
-  <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
-  <div class="absolute bottom-4 left-4 text-left">
-    <h1 class="text-lg font-semibold text-white">{props.title}</h1>
-    <p class="mt-2 text-sm text-gray-300">
+  <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+  <div className="absolute bottom-4 left-4 text-left">
+    <h1 className="text-lg font-semibold text-white">{props.title}</h1>
+    <p className="mt-2 text-sm text-gray-300">
       {props.Plot}
     </p>
-    <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+    <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
       Watch▶️
     </button>
   </div>
