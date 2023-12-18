ed7b8e0736bc8620edb2a583cb273fb658461378
Deepak <deepak2603dk@gmail.com>
2023-12-19 00:15:01 +0530

    navbar

---
+++ redux/src/component/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+<>
+<div className="nav bg-slate-600 h-10 w-full  flex justify-between px-5 text-white items-center font-semibold">
+    <h1> REDUX TOOLKIT</h1>
+    <h1 className='pe-20'>cart(1)</h1>
+    </div>
+</>
+  )
+}
+
+export default Navbar
