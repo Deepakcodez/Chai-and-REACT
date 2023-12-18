766e0d3dd9bf39c07a0e18a247a1dc47b475fff7
Deepak <deepak2603dk@gmail.com>
2023-12-19 01:18:58 +0530

    products code onclick fun

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,13 +1,14 @@
 import products from './products.json'
 import './App.css'
 import Products from './component/Products'
+import Navbar from './component/Navbar'
 
 function App() {
 
   return (
     <>
+    <Navbar/>
     <div className="products flex flex-wrap">
-
     {products.map((product)=> (<Products key={product.id} {...product}/>))}
     </div>
     </>
--- redux/src/component/Products.jsx
+++ redux/src/component/Products.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-
+import {useDispatch} from 'react-redux'
+import { addItems } from '../redux/CartSlice';
 function Products(props) {
     
+   
+    const dispatch = useDispatch();
   return (
    <>
    <div className="card h-48 bg-slate-500 m-3 w-[15rem] rounded-lg shadow-lg p-11 grow ">
     <div className="name"> product name:{props.name}</div>
-     <button className='bg-blue-800 rounded-lg p-2  text-white'>add  to cart</button>
+     <button className='bg-blue-800 rounded-lg p-2  text-white'
+     onClick={(e)=>dispatch(addItems({name:props.name,price:props.price}))}
+     >add  to cart</button>
    </div>
    </>
   )
