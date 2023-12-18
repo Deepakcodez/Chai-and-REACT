44b59a0ad7d5391e4cb8c808b1b79db77eb7c0a6
Deepak <deepak2603dk@gmail.com>
2023-12-19 01:17:18 +0530

    navbar

--- redux/src/component/Navbar.jsx
+++ redux/src/component/Navbar.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import { useSelector } from 'react-redux';
+
+const selectCart = (state) => state.cart;
 
 function Navbar() {
+  const cart = useSelector(selectCart);
+  const totalPrice = cart.reduce((a, b) => a + parseFloat(b.price), 0);
+
   return (
-<>
-<div className="nav bg-slate-600 h-10 w-full  flex justify-between px-5 text-white items-center font-semibold">
-    <h1> REDUX TOOLKIT</h1>
-    <h1 className='pe-20'>cart(1)</h1>
-    </div>
-</>
-  )
+    <>
+      <div className="nav bg-slate-600 h-10 w-full flex justify-between px-5 text-white items-center font-semibold">
+        <h1> REDUX TOOLKIT</h1>
+        <h1 className='pe-20'>cart({cart.length}) {totalPrice}</h1>
+      </div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
