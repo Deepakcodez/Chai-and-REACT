9bd0af77596823ab230ba19be98783c950a84718
Deepak <deepak2603dk@gmail.com>
2023-12-23 15:57:16 +0530

    code splitting

--- codeSplitting/src/App.jsx
+++ codeSplitting/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Suspense } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+//it is method of using fun from other folder
+try {
+  import("./component/SomeFunction").then((result) => {
+    console.warn(result.add(3, 4));
+  });
+} catch (error) {
+  console.error("Error during dynamic import:", error);
 }
 
-export default App
+const HeavyTask = React.lazy(() => import("./component/SomeheavyComp"));
+
+function App() {
+  return (
+    <>
+      <Suspense fallback={<div>loading.......</div>}>
+        <HeavyTask/>
+      </Suspense>
+    </>
+  );
+}
+
+export default App;
---
+++ codeSplitting/src/component/SomeheavyComp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ const SomeheavyComp = () => {
+  return (
+   
+    <>
+        <h1>perform heavy task</h1>
+    </>
+  )
+}
+export default SomeheavyComp
